bb981b3cf0f7d4c8f4d90107b53213e2dc7b0b44
ohud <ohud@cypress.com>
2019-09-18 18:18:49 +0300

    Implemented few APIs for NMEA0183

--- Tracker.cydsn/main.c
+++ Tracker.cydsn/main.c
@@ -10,11 +10,12 @@
  * ========================================
 */
 #include "project.h"
+#include <stdlib.h>
 
 #define NMEA_MAX_SIZE       82
 
 char NMEA_buffer[NMEA_MAX_SIZE];
-char NMEA_GPRMC[NMEA_MAX_SIZE];
+char NMEA_GPRMC[NMEA_MAX_SIZE] = "GPRMC";
 
 uint8 NMEA_pointer;
 
@@ -32,7 +33,7 @@ CY_ISR(GPS_receive)
         break;
         
         case 0x0A:
-        NMEA_handle_packet();
+        NMEA_handle_packet(&NMEA_buffer, &NMEA_GPRMC);
         break;
     }
 }
@@ -46,7 +47,6 @@ int main(void)
 
     CySysWdtSetInterruptCallback(CY_SYS_WDT_COUNTER2, (cyWdtCallback) wake_up_handler);
     
-
     for(;;)
     {
         CySysPmDeepSleep();
@@ -58,27 +58,78 @@ void wake_up_handler()
 {
 }
 
-void NMEA_handle_packet()
+void NMEA_GetField(char *packet, uint8 field, char *result)
+{
+    uint8 i, n;
+    uint8 count = 0;
+    
+    // Search field
+    for (i = 0; (i < NMEA_MAX_SIZE) & (count < field); i++)
+    {
+        if (packet[i]==',') count++;
+    }
+    i++;
+    
+    // Measure field size
+    for (count = 0; count < NMEA_MAX_SIZE; count++)
+    {
+        if (packet[i + count] != 0x0D) break;
+        if (packet[i + count] != ',') break;
+    }    
+    
+    // Copy field to result
+    for (n = 0; n < count; n++)
+    {
+        result[n] = packet[i+n];
+    }    
+}
+
+void NMEA_handle_packet(char * packet, char * NMEA_data)
 {
     uint8 i;
     uint8 error = 0;
+    uint8 checksum = 0;
+    char string_checksum[3];
     
     // Check for receive errors
-    for(i = 0; i <= NMEA_MAX_SIZE; i++)
+    for(i = 0; i < NMEA_MAX_SIZE; i++)
     {
-        if (NMEA_buffer[i] == 0) 
+        if ((packet[i] < 32) & (packet[i] != 0x0D) & (packet[i] != 0x0A)) 
         {
             error++;
             break;
         }
+        if (packet[i] != 0x0A) break;
     }
+    
+    // Validate checksum and cut packet if no receive errors
+    if (!error)
+    {
+        // Find checksum field
+        for(i = 0; i < NMEA_MAX_SIZE; i++)
+        {
+            if (packet[i] == '*') break;
+        }
+        packet[i] = 0;
+        
+        string_checksum[0] = packet[i+1];
+        string_checksum[1] = packet[i+2];
+        string_checksum[2] = 0;
+        
+        // Calculate checksum and compare
+        while(*packet) checksum ^= *packet++;
+        if (checksum != atoi(string_checksum)) error++;
+    }   
     
     // Copy buffer to NMEA packet if no errors found
     if (!error)
     {
-        for(i = 0; i <= NMEA_MAX_SIZE; i++)
+        if ((packet[3] == NMEA_data[3]) & (packet[4] == NMEA_data[4]) & (packet[5] == NMEA_data[5]))
         {
-            NMEA_GPRMC[i] = NMEA_buffer[i];
+            for(i = 0; i < NMEA_MAX_SIZE; i++)
+            {
+                NMEA_data[i] = packet[i];
+            }
         }
     }
 }
